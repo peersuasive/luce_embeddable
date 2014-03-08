21ec57c23ce18aaf2837025d92cbea6f23e9a2ab
Christophe Berbizier (dbdl) <cberbizier@peersuasive.com>
2014-03-08 04:32:05 +0100

    Demo: updated for Android compatibility

--- example/Demo.lua
+++ example/Demo.lua
@@ -1,5 +1,16 @@
 #!/usr/bin/env lua
 
+print("arg")
+local android = os.getenv('ANDROID_DATA') and true
+
+local out = (android) and io.open("/data/data/org.peersuasive.luce.demo/outdebug.log", "wb") or io.stdout
+local function log(msg, ...)
+    local msg = (msg or "").."\n"
+    out:write(string.format(msg, ...))
+    out:flush()
+end
+
+log("is android ? %s", android)
 local args = {...}
 
 local demos = {
@@ -14,12 +25,12 @@ local animations = {
 }
 local demo = "GlyphDemo"
 local usage = function()
-    print("Usage: Demo.lua [options] [demo]")
-    print(" demo: one of GlypDemo, LinesDemo (default: GlyphDemo)")
-    print("options:")
-    print(" -d <animation> [-d ...]    disable selected animation")
-    print("                can be one of rotation, position, shear, size or all (default: all enabled)")  
-    print()
+    log("Usage: Demo.lua [options] [demo]")
+    log(" demo: one of GlypDemo, LinesDemo (default: GlyphDemo)")
+    log("options:")
+    log(" -d <animation> [-d ...]    disable selected animation")
+    log("                can be one of rotation, position, shear, size or all (default: all enabled)")  
+    log()
 end
 if(#args>0)then
     for i,o in next, args do
@@ -37,57 +48,103 @@ if(#args>0)then
             elseif( animations[a] ) then
                 animations[a] = false
             else 
-                print(string.format("unknown animation: %s, ignoring", a))
+                log(string.format("unknown animation: %s, ignoring", a))
             end
             args[i+1] = ""
         else
-            print(string.format("unknown option: %s\n", o))
+            log(string.format("unknown option: %s\n", o))
             usage()
             os.exit(1)
         end
     else
         if not(""==o)then
         if not(demos[o])then
-            print(string.format("Unknow demo '%s', using default (%s)", o, demo))
+            log(string.format("Unknow demo '%s', using default (%s)", o, demo))
         end
         end
         demo = demos[o] or "GlyphDemo"
     end
     end
 end
-
+log("1")
 local luce = require"luce"()
-
+log("2", luce)
 local mainWindow = luce:JUCEApplication()
-local dw = luce:DocumentWindow("Document Window")
-local mc = luce:MainComponent("Main Component")
-mc:setSize{1,1}
+log("3", mainWindow)
 
+local dw, mc, demoHolder = nil, nil, nil
+local function start()
+    log("start")
+    local dw = luce:DocumentWindow("Document Window")
+    log("4")
+    local mc = luce:MainComponent("Main Component")
+    log("5")
+    mc:setSize{1,1}
+    log("6")
+    ---[[
+    local demoHolder = require"DemoHolder"(demo)
+    log("7")
+    demoHolder.animations.animateRotation = animations.rotation
+    demoHolder.animations.animatePosition = animations.position
+    demoHolder.animations.animateShear    = animations.shear
+    demoHolder.animations.animateSize     = animations.size
 
-local demoHolder = require"DemoHolder"(demo)
+    log("8")
+    mc:resized(function()
+        demoHolder:setBounds( luce:Rectangle(mc:getLocalBounds()) )
+    end)
+    --]]
+    --[[
+    mc:paint(function(g)
+        log("***************** paint!!!!!!!!!!!!!!!!!!!!!!!!!")
+        log("1")
+        g:fillAll(luce:Colour(0xffeeddff))
+        log("2")
+        g:setFont(16.0)
+        log("3")
+        g:setColour(luce:Colour(luce.Colours.black))
+        log("4")
+        g:drawText("Hello World!", mc:getLocalBounds(), luce.JustificationType.centred, true);
+        log("5")
+    end)
+    --]]
+    log("9")
+    return dw, mc, demoHolder
+end
 
-demoHolder.animations.animateRotation = animations.rotation
-demoHolder.animations.animatePosition = animations.position
-demoHolder.animations.animateShear    = animations.shear
-demoHolder.animations.animateSize     = animations.size
-
-mc:resized(function()
-    demoHolder:setBounds( luce:Rectangle(mc:getLocalBounds()) )
-end)
+--if not(android) then dw, mc, demoHolder = start() end
 
 mainWindow:initialise(function(...)
+    log("initialise")
+    dw, mc, demoHolder = start()
+    ---[[
+    log("add and make visible...")
     mc:addAndMakeVisible( demoHolder )
+    log("set bounds...")
     demoHolder:setBounds{ 0, 0, 800, 600 }
+    log("start demo...")
     demoHolder:startDemo()
+    --]]
+    log("set content owned...")
     dw:setContentOwned( mc, true )
+    log("centre with size...")
     dw:centreWithSize{800, 600}
+    log("set visible... %s", dw.setVisible)
     dw:setVisible(true)
+    log("GO!!!!!!")
     return dw
 end)
 
+mainWindow:resumed(function(...)
+    log("lua: resuming!")
+end)
+
+local stop_now = false
 mainWindow:systemRequestedQuit(function(...)
     mainWindow:shutdown()
     mainWindow:quit()
+    stop_now = true
+    --if(android)then luce:shutdown() end
 end)
 luce:start(mainWindow)
-luce:shutdown()
+if not(android) then luce:shutdown() end
